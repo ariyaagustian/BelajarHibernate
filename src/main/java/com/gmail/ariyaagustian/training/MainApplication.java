e0b060b8e6512e8dee2ec73eee9cd4e8f95b6bd7
ariyaagustian <ariyaagustian@gmail.com>
2019-01-16 10:11:29 +0700

    Commit 2

---
+++ src/main/java/com/gmail/ariyaagustian/training/MainApplication.java
@@ -0,0 +1,10 @@
+package com.gmail.ariyaagustian.training;
+
+public class MainApplication {
+
+
+
+    public static void main(String[] args) {
+        System.out.println("Hello Unikom");
+    }
+}
